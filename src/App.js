bd6985bba6df3a1b2036ad7f66e807b955de58e1
Heidi <emhp009@gmail.com>
2024-01-29 10:37:57 +0100

    some additions
    Navbar, then added to app, assets folder. global index.css,

--- src/App.js
+++ src/App.js
@@ -2,10 +2,12 @@ import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import WarmJobs from "./pages/WarmJobs";
 import ColdJobs from "./pages/ColdJobs";
+import ApplyPage from "./pages/ApplyPage";
 
 function App() {
   return (
     <div>
+      <NavBar />
       hello we are reactors
       <Routes>
         <Route path="/warm" element={<WarmJobs />} />
---
+++ src/components/NavBar.js
@@ -0,0 +1,41 @@
+import React from "react";
+import Logo from "../Assets/NavLogo copy.png";
+import "../index.css";
+
+function NavBar() {
+  return (
+    <div
+      style={{
+        backgroundColor: "#2f0742",
+        minHeight: 50,
+        paddingLeft: 10,
+        paddingRight: 20,
+        paddingTop: 10,
+        display: "flex",
+        fontSize: "1.3em",
+        color: "#FFD801",
+        fontFamily: "ItikKecil",
+        gap: 2,
+      }}
+    >
+      <img src={Logo} alt="NavLogo" style={{ height: 50, marginTop: -5 }} />
+      <div
+        style={{
+          marginLeft: "auto",
+          display: "flex",
+          gap: 25,
+          paddingLeft: 10,
+          paddingRight: 20,
+          paddingTop: 10,
+          color: "#09D0D8",
+        }}
+      >
+        <p>Home</p>
+        <p>Find a job</p>
+        <p>About us</p>
+      </div>
+    </div>
+  );
+}
+
+export default NavBar;
