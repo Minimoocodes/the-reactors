193c687b44275bff02f710a69ec1a5af8d46ebd8
Cyloon <cyloon@gmail.com>
2024-01-29 22:02:50 +0100

    added text

--- src/pages/WarmOrColdPage.js
+++ src/pages/WarmOrColdPage.js
@@ -5,11 +5,19 @@ const WarmOrCold = () => {
   const warmOrColdArray = ["warm", "cold"];
 
   return (
-    <div className="WarmOrCold_wrap">
-      {warmOrColdArray.map((warmOrColdDisplay, index) => (
-        <WarmOrColdCard key={index} warmOrColdDisplay={warmOrColdDisplay} />
-      ))}
-    </div>
+    <>
+      <div className="WarmOrCold_wrap">
+        <h1>Do you prefer to work in a warm or cold climate?</h1>
+      </div>
+      <div className="WarmOrCold_wrap">
+        <h2>Click to see what jobs are available</h2>
+      </div>
+      <div className="WarmOrCold_wrap">
+        {warmOrColdArray.map((warmOrColdDisplay, index) => (
+          <WarmOrColdCard key={index} warmOrColdDisplay={warmOrColdDisplay} />
+        ))}
+      </div>
+    </>
   );
 };
 
