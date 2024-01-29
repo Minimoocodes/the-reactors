b33ffa3d035075fbdf2e167b84f5e2c1ea694739
Cyloon <cyloon@gmail.com>
2024-01-29 12:24:14 +0100

    created files and dependencies

---
+++ src/components/WarmOrColdCard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const WarmOrColdCard = ({ warmOrCold }) => {
+  const warmOrCold = [warm, cold];
+};
+
+return (
+  <div className="WarmOrColdCard_wrap">
+    <div className="WarmOrColdCard_content">
+      <h1>{warmOrColdDisplay}</h1>
+    </div>
+  </div>
+);
+
+export default WarmOrColdCard;
--- src/pages/WarmOrColdPage.js
+++ src/pages/WarmOrColdPage.js
@@ -1,7 +1,14 @@
 import React from "react";
+import WarmOrColdCard from "../components/WarmOrColdCard";
 
 const WarmOrCold = () => {
-  return <div className="warmorcold-wrap"></div>;
+  return (
+    <div className="warmorcold-wrap">
+      {warmOrCold.map((warmOrColdDisplay) => (
+        <WarmOrColdCard warmOrColdDisplay={warmOrColdDisplay} />
+      ))}
+    </div>
+  );
 };
 
 export default WarmOrCold;
