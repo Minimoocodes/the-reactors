e4ed13e6a9eced61e73346b638cd37c1eeff3f63
Minimoocodes <jaemin.shim14@gmail.com>
2024-01-31 21:08:01 +0100

    more cities added

--- src/pages/ColdJobs.js
+++ src/pages/ColdJobs.js
@@ -1,20 +1,46 @@
 import React from "react";
 import CityCard from "../components/CityCard.js";
+import JobListBox from "../components/JobListBox.js";
 
-const ColdJobs = ({ jsonData, apply }) => {
-  const coldCities = ["Stockholm", "New York", "Paris", "Seoul", "London"];
+const ColdJobs = ({ jsonData, savedJobs, setSavedJobs }) => {
+  const coldCities = [
+    "Stockholm",
+    "New York",
+    "Paris",
+    "Seoul",
+    "London",
+    "Reykjavik",
+    "Toronto",
+    "Beijing",
+  ];
   const images = {
     Stockholm: `https://i.natgeofe.com/n/90e37eca-f03a-45c9-a0b5-67609d37fd0b/sto_3x4.jpg`,
     "New York": `https://image.cnbcfm.com/api/v1/image/106268734-1574876711571gettyimages-1059614218.jpeg?v=1576856860`,
     Paris: `https://res.klook.com/image/upload/Mobile/City/swox6wjsl5ndvkv5jvum.jpg`,
     Seoul: `https://cms.finnair.com/resource/blob/681052/5647b8b6803ecae2b51363d64e98ff3a/seoul-main-data.jpg?impolicy=crop&width=2989&height=2242&x=217&y=0`,
     London: `https://cdn-imgix.headout.com/media/images/db48a90d442343a3153acc31fa839a50-Best%20time%20to%20visit%20london%20-%20Tower%20Bridge.jpg`,
+    Reykjavik: `https://i.pinimg.com/736x/80/27/b2/8027b2b9c1649471cfc13cd16000a271.jpg`,
+    Toronto: `https://media.istockphoto.com/id/626545182/sv/foto/toronto-skyline-with-purple-light-toronto-ontario-canada.jpg?s=612x612&w=0&k=20&c=vu41k0gGbiNeQ8TrB7ghrKEk0K599bQbVsvQMnE4aOo=`,
+    Beijing: `https://lh3.googleusercontent.com/proxy/0i7nasGum5BXBhSSdsBPqbKmnZkKFc_yqDoOyeCzwGDch-tRft-cVOqNUUFgZEpoiKI8ueYEdydd85MvvMYssmsz0xfYzxHoUSh6Zx3L0vJH3It7v2sPbnyPX1TkJLR2UQ`,
   };
+  const jobList = Object.entries(jsonData);
 
   return (
-    <div className="warm_wrap">
+    <div className="cities_wrap">
       {coldCities.map((city) => (
-        <CityCard city={city} images={images} />
+        <div className="cities_content">
+          <div className="warm_wrap">
+            <CityCard city={city} images={images} />
+          </div>
+          <div className="joblist_wrap">
+            <JobListBox
+              city={city}
+              job={jobList}
+              savedJobs={savedJobs}
+              setSavedJobs={setSavedJobs}
+            />
+          </div>
+        </div>
       ))}
     </div>
   );
--- src/pages/WarmJobs.js
+++ src/pages/WarmJobs.js
@@ -3,13 +3,25 @@ import CityCard from "../components/CityCard";
 import JobListBox from "../components/JobListBox";
 
 const WarmJobs = ({ jsonData, savedJobs, setSavedJobs }) => {
-  const warmCities = ["Dubai", "Los Angeles", "Cebu", "Bali", "Sydney"];
+  const warmCities = [
+    "Dubai",
+    "Los Angeles",
+    "Cebu",
+    "Bali",
+    "Sydney",
+    "Miami",
+    "Singapore",
+    "Rio de Janeiro",
+  ];
   const images = {
     Dubai: `https://travelbird-images.imgix.net/f2/0b/f20bce73731062f021a0c62b7353fa76?auto=compress%2Cformat&crop=faces%2Cedges%2Ccenter&fit=crop&h=1144&upscale=true&w=828`,
     "Los Angeles": `https://a.cdn-hotels.com/gdcs/production46/d1416/699535be-fc0a-4ece-9504-dab07867e26c.jpg?impolicy=fcrop&w=800&h=533&q=medium`,
     Cebu: `https://www.citrineland.com/wp-content/uploads/2021/02/bantayan-island-cebu-best-beach-1.jpg`,
     Bali: `https://content.api.news/v3/images/bin/59806f50929d8578cca0a01e7f90ea78`,
     Sydney: `https://www.aircalin.com/sites/default/files/styles/hero_banner_mobile/public/2022-08/Sydney-min.jpg?h=8edabf11&itok=KPO8L4Ug`,
+    Miami: `https://www.miamiandbeaches.com/getmedia/dd15c70f-a3d1-4cb3-860c-600a3392c0b3/Pink_lifeguard_tower_Miami_Beach_green_flag_1440x900.jpg`,
+    Singapore: `https://www.telegraph.co.uk/content/dam/Travel/Destinations/Asia/Singapore/singapore-destination-guide-lead.jpg`,
+    "Rio de Janeiro": `https://i.natgeofe.com/n/560b293d-80b2-4449-ad6c-036a249d46f8/rio-de-janeiro-travel_3x4.jpg`,
   };
 
   // to convert the json data into array
