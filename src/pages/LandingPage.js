a683f1b90d8b4fbb8b6a179e984b0577a047c85c
Cyloon <cyloon@gmail.com>
2024-02-01 16:01:12 +0100

    closer to finnished
    Co-authored-by: Petra Cuturdic <PoppyRed91@users.noreply.github.com>

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,3 +1,5 @@
+import landinglogo from "../Assets/reactorslandinglogo_transp.png";
+
 export default function LandingPage({ navigate }) {
   return (
     <div
@@ -10,8 +12,18 @@ export default function LandingPage({ navigate }) {
         height: "100vh",
       }}
     >
-      <div>
-        <img src="/images/reactorslandinglogo.png" />
+      <div className="landing-page__wrapper">
+        <div className="title__wrapper">
+          <img
+            src={landinglogo}
+            alt="thereactorslogo"
+            className="landing-page__logo"
+          />
+          <div className="landing-page__text"></div>
+        </div>
+        <div className="looking-for-job__wrapper">
+          <button className="button">Check our jobs here!</button>
+        </div>
       </div>
     </div>
   );
