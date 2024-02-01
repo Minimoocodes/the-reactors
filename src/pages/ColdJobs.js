8d13924537ac1073b6fcadc5582ad5fa365382c9
Minimoocodes <jaemin.shim14@gmail.com>
2024-02-01 13:04:55 +0100

    joblist box imported to cold jobs

--- src/pages/ColdJobs.js
+++ src/pages/ColdJobs.js
@@ -1,5 +1,6 @@
 import React from "react";
 import CityCard from "../components/CityCard.js";
+import JobListBox from "../components/JobListBox.js";
 
 const ColdJobs = ({ jsonData, savedJobs, setSavedJobs }) => {
   const coldCities = [
