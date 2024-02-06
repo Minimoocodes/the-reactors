96b023c34dcfc18c9f38e09ff0ab0b0ee4519bed
Cyloon <cyloon@gmail.com>
2024-02-06 13:59:50 +0100

    adjusted css and added comments

--- src/components/WarmOrColdCard.js
+++ src/components/WarmOrColdCard.js
@@ -22,17 +22,22 @@ const coldCities = [
   "Copenhagen",
 ];
 
+//Warm or cold choice card. The WarmOrColdCard function gets warmOrColdDisplay parameter (warmorcoldArray from WarmorColdPage.js)
 const WarmOrColdCard = ({ warmOrColdDisplay }) => {
+  //warmorcoldimages, an object with warm and cold inside that is links to different images to be used as backgrounds
   const warmorcoldimages = {
     warm: "/image/warm_weather.png",
     cold: "/image/cold_weather.png",
   };
 
+  //isWarm is true when warmOrColdDisplay equals "warm"
   const isWarm = warmOrColdDisplay === "warm";
   return (
+    //card is wrapped in link and then depending if warmOrColdDisplay is warm or cold then /warm or /cold
     <Link to={`/${warmOrColdDisplay}`}>
       <div
         className="WarmOrColdCard_wrap"
+        //depending if warmOrColdDisplay is warm or cold, warmorcoldimages object is chosen with the different link
         style={{
           backgroundImage: `url(
         "${warmorcoldimages[warmOrColdDisplay]}"
@@ -40,15 +45,17 @@ const WarmOrColdCard = ({ warmOrColdDisplay }) => {
         }}
       >
         <div className="WarmOrColdCard_content">
+          {/* if statement. if isWarm is true then map warmCities array and make a list with each city
+          otherwise map coldCities and make a list of those cities in that array */}
           {isWarm
             ? warmCities.map((city) => (
-                <ul>
-                  <li>{city}</li>
+                <ul className="findajob_ul">
+                  <li className="findajob_li">{city}</li>
                 </ul>
               ))
             : coldCities.map((city) => (
-                <ul>
-                  <li>{city}</li>
+                <ul className="findajob_ul">
+                  <li className="findajob_li">{city}</li>
                 </ul>
               ))}
         </div>
